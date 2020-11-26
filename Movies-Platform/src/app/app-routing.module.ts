21c950af4f98a84375561405d25a48999b5a62a3
Iakovos Lampadakis <iakovoslampadakis@gmail.com>
2020-11-26 23:45:00 +0200

    Change buttons style/add profile/details Component

--- Movies-Platform/src/app/app-routing.module.ts
+++ Movies-Platform/src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { HomeComponent } from './home';
 import { AuthGuard } from './_helpers';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+const profileModule = () => import('./profile/profile.module').then(x => x.ProfileModule);
 const moviesModule = () => import('./movies/movies.module').then(x => x.MoviesModule);
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    
+    { path: 'profile', loadChildren: profileModule, canActivate: [AuthGuard] },
     { path: 'movies', loadChildren: moviesModule, canActivate: [AuthGuard] },
     { path: 'account', loadChildren: accountModule },
 
--- Movies-Platform/src/app/profile/details/details.component.ts
+++ Movies-Platform/src/app/profile/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-details',
@@ -6,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.less']
 })
 export class DetailsComponent implements OnInit {
+  user: Observable<User>;
+  currentUser: User;
+  error: string;
 
-  constructor() { }
+  constructor(private userService: UserService) { 
+    this.user = this.userService.getUser();
+    this.user.subscribe(
+      (user) => (this.currentUser = user),
+      (error) => {
+        this.error = error;
+      }
+    );
+  }
+
+  
 
   ngOnInit(): void {
   }
